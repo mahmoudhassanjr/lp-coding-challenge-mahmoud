85a44160c0d6a92409eb729a0bbf79ead5bef68f
mahmoudhassanjr <mh.mshinda@gmail.com>
2025-04-09 03:33:28 +1000

    New line to display the final line properly.

--- src/main/java/com/littlepaychallenge/mahmoud/service/FileService.java
+++ src/main/java/com/littlepaychallenge/mahmoud/service/FileService.java
@@ -76,6 +76,8 @@ public class FileService {
                 }
                 writer.flush();
             }
+
+            System.out.println();
             
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
